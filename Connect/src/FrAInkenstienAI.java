311add9a10dddaaadd9d56cf6cc133d40d03ae2c
Luke Lohden <lukasl@mac.com>
2016-10-17 17:24:54 -0700

    Reworked basic AI to make a random move on its turn.

--- Connect/src/FrAInkenstienAI.java
+++ Connect/src/FrAInkenstienAI.java
@@ -1,6 +1,9 @@
 import connectK.CKPlayer;
 import connectK.BoardModel;
+
 import java.awt.Point;
+import java.lang.Math;
+
 
 public class FrAInkenstienAI extends CKPlayer {
 
@@ -11,11 +14,25 @@ public class FrAInkenstienAI extends CKPlayer {
 
 	@Override
 	public Point getMove(BoardModel state) {
-		for(int i=0; i<state.getWidth(); ++i)
-			for(int j=0; j<state.getHeight(); ++j)
-				if(state.getSpace(i, j) == 0)
-					return new Point(i,j);
+		while (state.hasMovesLeft())
+		{
+			int i = (int)(Math.random() * state.getWidth() );
+			int j = (int)(Math.random() * state.getHeight() );
+			while (state.getSpace(i,j) != 0)
+			{
+				i = (int)(Math.random() * state.getWidth() );
+				j = (int)(Math.random() * state.getHeight() );
+			}
+			return new Point(i,j);
+		}
 		return null;
+
+		
+//		for(int i=0; i<state.getWidth(); ++i)
+//			for(int j=0; j<state.getHeight(); ++j)
+//				if(state.getSpace(i, j) == 0)
+//					return new Point(i,j);
+		
 	}
 
 	@Override
